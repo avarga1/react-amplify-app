530bc773dcb61db735f68fd6a3312ede058688b7
Austin Varga <austinvarga1@protonmail.com>
2023-01-10 19:34:06 -0800

    Removed landing for trouble shooting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Sidebar from './Sidebar';
-import Landing from './Landing';
+
 import Contact from './Contact';
 import BlankSpace from './BlankSpace';
 
@@ -13,19 +13,7 @@ function App() {
         <Header/>
         <Main />
         <Sidebar />
-        <div id="home">
-          <Landing />
-        </div>
-        <div id="about">
-          {/* Your "About" component goes here */}
-        </div>  
-        <div id="projects">
-          {/* Your "Projects" component goes here */}
-        </div>
-        <BlankSpace height="350px" />
-        <div id="contact">
-          <Contact />
-        </div>
+        <Contact />
     </div>
   );
 }
