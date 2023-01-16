43b0ba157c75730bc00f48a3dc74fe0fbd290396
Austin Varga <austinvarga1@protonmail.com>
2023-01-16 13:09:50 -0800

    less

--- src/App.js
+++ src/App.js
@@ -15,25 +15,33 @@ import Contact from './Footer/Contact';
 function App() {
   return (
     <div className="app-container">
-        
+
         <NavBar />
         <div id="home"></div>
-        <BlankSpace height={100} />
+        
         <Logo />
         <div id="about"></div>
         <Header />
         <AboutMe />  
-        <BlankSpace height={100} />
+       
         <div id="projects"></div>
         <Portfolio/>   
-        <BlankSpace height={300} />
+
         <Skills/>
-        <BlankSpace height={900} />
+ 
         <Contact/>
         <div id="contact"></div>
     </div>
   );
 }
+
+
+/*
+        <div className="hexagons-container" style={{pointerEvents: "none"}}>
+            <HexBG />
+        </div>
+        <BlankSpace height={100} />
+*/
 
 
 export default App;
